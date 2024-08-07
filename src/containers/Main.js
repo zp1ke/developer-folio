a730167d98ca7f67d6e335e7bf338f01f3ac1f6e
Sp1ke <miche.atucha@gmail.com>
2024-08-07 17:54:49 -0500

    content focused

--- src/containers/Main.js
+++ src/containers/Main.js
@@ -2,19 +2,19 @@ import React, {useEffect, useState} from "react";
 import Header from "../components/header/Header";
 import Greeting from "./greeting/Greeting";
 import Skills from "./skills/Skills";
-import StackProgress from "./skillProgress/skillProgress";
+// import StackProgress from "./skillProgress/skillProgress";
 import WorkExperience from "./workExperience/WorkExperience";
-import Projects from "./projects/Projects";
-import StartupProject from "./StartupProjects/StartupProject";
-import Achievement from "./achievement/Achievement";
-import Blogs from "./blogs/Blogs";
+// import Projects from "./projects/Projects";
+// import StartupProject from "./StartupProjects/StartupProject";
+// import Achievement from "./achievement/Achievement";
+// import Blogs from "./blogs/Blogs";
 import Footer from "../components/footer/Footer";
-import Talks from "./talks/Talks";
-import Podcast from "./podcast/Podcast";
-import Education from "./education/Education";
+// import Talks from "./talks/Talks";
+// import Podcast from "./podcast/Podcast";
+// import Education from "./education/Education";
 import ScrollToTopButton from "./topbutton/Top";
-import Twitter from "./twitter-embed/twitter";
-import Profile from "./profile/Profile";
+// import Twitter from "./twitter-embed/twitter";
+// import Profile from "./profile/Profile";
 import SplashScreen from "./splashScreen/SplashScreen";
 import {splashScreen} from "../portfolio";
 import {StyleProvider} from "../contexts/StyleContext";
@@ -53,17 +53,17 @@ const Main = () => {
             <Header />
             <Greeting />
             <Skills />
-            <StackProgress />
-            <Education />
+            {/* <StackProgress /> */}
+            {/* <Education /> */}
             <WorkExperience />
-            <Projects />
-            <StartupProject />
-            <Achievement />
-            <Blogs />
-            <Talks />
-            <Twitter />
-            <Podcast />
-            <Profile />
+            {/* <Projects /> */}
+            {/* <StartupProject /> */}
+            {/* <Achievement /> */}
+            {/* <Blogs /> */}
+            {/* <Talks /> */}
+            {/* <Twitter /> */}
+            {/* <Podcast /> */}
+            {/* <Profile /> */}
             <Footer />
             <ScrollToTopButton />
           </>
